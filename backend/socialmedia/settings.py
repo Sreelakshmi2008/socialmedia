fb7aea87d33e5f75e678ba9b7a9ddbe2828b6b8e
Sreelakshmi2008 <sreetb2008@gmail.com>
2023-12-14 18:16:59 +0530

    editing requirements

--- backend/socialmedia/settings.py
+++ backend/socialmedia/settings.py
@@ -144,10 +144,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

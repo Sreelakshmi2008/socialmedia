c8be450ebef122c6a42b140f237d392d9cfafcee
Sreelakshmi2008 <sreetb2008@gmail.com>
2023-12-14 20:55:17 +0530

    adding changes in settings

--- backend/socialmedia/settings.py
+++ backend/socialmedia/settings.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = 'socialmedia.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
